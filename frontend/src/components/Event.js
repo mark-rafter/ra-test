f63b7f393826c797d3552d38f5de62d9accd5505
mark <38855689+mark-rafter@users.noreply.github.com>
2022-01-27 22:27:28 +0000

    cool

--- frontend/src/components/Event.js
+++
@@ -1,7 +0,0 @@
-export default function Event(id, title, date, startTime, endTime) {
-    return <li key={id}>
-        <p>{new Date(date).toLocaleDateString()}</p>
-        <p>{title}</p>
-        <p>{new Date(startTime).toLocaleTimeString()} &mdash; {new Date(endTime).toLocaleTimeString()}</p>
-    </li>;
-}
--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -1,28 +1,59 @@
 import { useQuery, gql } from "@apollo/client";
-import Event from './Event';
 
-export const EVENTS = gql`
+const EVENTS = gql`
   query GetEvents($from: DateTime!, $to: DateTime!) {
     events(dateFrom: $from, dateTo: $to) {
       id
       title
       date
+      venue { 
+        name
+      }
+      artists {
+        name
+      }
+      promoters { 
+        name
+      }
       startTime
       endTime
     }
   }
 `;
 
+function Event({ id, title, date, venue, artists, promoters, startTime, endTime }) {
+  return <li key={id}>
+    <p>{new Date(date).toLocaleDateString()}</p>
+    <p>{venue.name}</p>
+    <p>{title}</p>
+    <p>{new Date(startTime).toLocaleTimeString()} &mdash; {new Date(endTime).toLocaleTimeString()}</p>
+    <p>Artists:
+      <ul>
+        {artists.map((a) => (
+          <li>{a.name}</li>
+        ))}
+      </ul>
+    </p>
+    <p>Promoters:
+      <ul>
+        {promoters.map((p) => (
+          <li>{p.name}</li>
+        ))}
+      </ul>
+    </p>
+  </li>;
+}
+
 export default function EventList({ from, to }) {
-    const { loading, error, data } = useQuery(EVENTS, {
-        variables: { from, to },
-    });
+  const { loading, error, data } = useQuery(EVENTS, {
+    variables: { from, to },
+  });
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
-    return <ul>
-        {data.events.map(({ id, title, date, startTime, endTime }) => (
-            Event(id, title, date, startTime, endTime)
-        ))}</ul>;
+  return <ul>
+    {data.events.map((e) => (
+      Event(e)
+    ))}</ul>;
 }
